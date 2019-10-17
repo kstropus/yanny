93217f705d6fd934a0753e73edf165ce61008099
Keith Stropus <sizzlefist@hotmail.com>
2019-10-16 22:26:09 -0700

    printf start

--- include/common/stddef.h
+++ include/common/stddef.h
@@ -1,8 +1,8 @@
 /* Copyright (C) 1989-2019 Free Software Foundation, Inc.
 
-This file is part of GCC.
+//This file is part of GCC.
 
-GCC is free software; you can redistribute it and/or modify
+//GCC is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 3, or (at your option)
 any later version.
--- src/common/stdio.c
+++ src/common/stdio.c
@@ -1,5 +1,6 @@
 #include <common/stdio.h>
 #include <common/stdlib.h>
+#include <common/stdarg.h>
 #include <kernel/uart.h>
 
 char getc(void) {
@@ -20,10 +21,6 @@ void gets(char * buf, int buflen) {
     int i;
     char c;
 
-	c = getc();
-
-	while(c != '\0' && c != '\r' && c != '\n')
-
     // Leave a spot for null char in buffer
     for (i = 0; (c = getc()) != '\r' && buflen > 1; i++, buflen--) {
         //putc(c);
@@ -36,4 +33,56 @@ void gets(char * buf, int buflen) {
     }
     else
         buf[buflen-1] = '\0';
+}
+
+int printf(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    int n = 0;
+
+    int state = 0;
+
+    while(format[n] != '\0')
+    {
+        switch(state)
+        {
+            case 0:
+                if(format[n] != '%')
+                    putc(format[n]);
+                else
+                    state = 1;
+                break;
+            case 1:
+                if(format[n] == '%')
+                {
+                    putc('%');
+                    state = 0;
+                }
+                else
+                {
+                    switch(format[n])
+                    {
+                        case 's':
+                            break;
+                        case 'd':
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+
+        n++;
+    }
+
+    va_end(args);
+
+    return 0;
 }
